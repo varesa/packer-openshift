b4bfeeddc44018d53ac40dadccd634cfa438e2e1
Esa Varemo <esa@kuivanto.fi>
2018-11-10 13:19:45 +0200

    Allow build.sh to take the template as a parameter

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+export VSPHERE_ADDRESS=$(vault kv get /kv/packer/vsphere | jq -r '.data.address')
 export VSPHERE_USERNAME=$(vault kv get /kv/packer/vsphere | jq -r '.data.username')
 export VSPHERE_PASSWORD=$(vault kv get /kv/packer/vsphere | jq -r '.data.password')
 
 export ROOT_PASSWORD=$(vault kv get /kv/packer/vm | jq -r '.data.password')
 
-packer build -var "vsphere_password=$VSPHERE_PASSWORD" -var "guest_password=$ROOT_PASSWORD" /templates/clone.json
+if [ ! -f $1 ]
+then
+    echo Please provide template as a parameter
+    exit 1
+fi
+
+packer build \
+    -var "vsphere_address=$VSPHERE_ADDRESS" -var "vsphere_username=$VSPHERE_USERNAME" -var "vsphere_password=$VSPHERE_PASSWORD" \
+    -var "guest_password=$ROOT_PASSWORD" \
+    $1
